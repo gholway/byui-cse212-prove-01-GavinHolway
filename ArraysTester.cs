2dc07b71cdc6b5ff95c7cc2fefc67b9f57b212ea
gholway <131705574+gholway@users.noreply.github.com>
2024-04-27 18:43:00 -0600

    Add files via upload

---
+++ ArraysTester.cs
@@ -0,0 +1,73 @@
+namespace prove_01;
+
+public static class ArraysTester {
+    /// <summary>
+    /// Entry point for the Prove 1 tests
+    /// </summary>
+    public static void Run() {
+        // Sample Test Cases (may not be comprehensive)
+        Console.WriteLine("\n=========== PROBLEM 1 TESTS ===========");
+        double[] multiples = MultiplesOf(7, 5);
+        Console.WriteLine($"<double>{{{string.Join(',', multiples)}}}"); // <double>{7, 14, 21, 28, 35}
+        multiples = MultiplesOf(1.5, 10);
+        Console.WriteLine($"<double>{{{string.Join(',', multiples)}}}"); // <double>{1.5, 3.0, 4.5, 6.0, 7.5, 9.0, 10.5, 12.0, 13.5, 15.0}
+        multiples = MultiplesOf(-2, 10);
+        Console.WriteLine($"<double>{{{string.Join(',', multiples)}}}"); // <double>{-2, -4, -6, -8, -10, -12, -14, -16, -18, -20}
+
+        Console.WriteLine("\n=========== PROBLEM 2 TESTS ===========");
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        RotateListRight(numbers, 1);
+        Console.WriteLine($"<List>{{{string.Join(',', numbers)}}}"); // <List>{9, 1, 2, 3, 4, 5, 6, 7, 8}
+        numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        RotateListRight(numbers, 5);
+        Console.WriteLine($"<List>{{{string.Join(',', numbers)}}}"); // <List>{5, 6, 7, 8, 9, 1, 2, 3, 4}
+        numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        RotateListRight(numbers, 3);
+        Console.WriteLine($"<List>{{{string.Join(',', numbers)}}}"); // <List>{7, 8, 9, 1, 2, 3, 4, 5, 6}
+        numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        RotateListRight(numbers, 9);
+        Console.WriteLine($"<List>{{{string.Join(',', numbers)}}}"); // <List>{1, 2, 3, 4, 5, 6, 7, 8, 9}
+    }
+    /// <summary>
+    /// This function will produce a list of size 'length' starting with 'number' followed by multiples of 'number'.  For 
+    /// example, MultiplesOf(7, 5) will result in: {7, 14, 21, 28, 35}.  Assume that length is a positive
+    /// integer greater than 0.
+    /// </summary>
+    /// <returns>array of doubles that are the multiples of the supplied number</returns>
+    private static double[] MultiplesOf(double number, int length)
+    {
+        multiCycle = 0;//number for loop count and calculating number timestable calculation
+        double[] multiTable = (number);//array for holding list with number times one as it's only inital entry
+        while(multiCycle < length)//loops until multicyle is equal to length
+        {
+            multiCycle += 1;//ends loop and is used to multiplay by for each loop for the array
+            if(multiCycle != 1)// this if statement prevents number times one from being present twice
+            {
+                multiTable.Add(number * multiCycle);//adds loop result to list
+            }
+        }
+        // TODO Problem 1 Start (don't forget to fill out the 01-prove-response.docx)
+        return multiTable; // returns list
+    }
+    
+    /// <summary>
+    /// Rotate the 'data' to the right by the 'amount'.  For example, if the data is 
+    /// <c>&lt;List&gt;{1, 2, 3, 4, 5, 6, 7, 8, 9}</c> and an amount is 3 then the list returned should be 
+    /// <c>&lt;List&gt;{7, 8, 9, 1, 2, 3, 4, 5, 6}</c>.  The value of amount will be in the range of <c>1</c> and 
+    /// <c>data.Count</c>.
+    /// <br /><br />
+    /// Because a list is dynamic, this function will modify the existing <c>data</c> list rather than returning a new list.
+    /// </summary>
+    private static void RotateListRight(List<int> data, int amount)
+    {
+        data = data.GetRange((9 - amount), 8);//because computers start counting at 0 1 is at index 0 and 9 at index 8 so 9 - amount puts the start of the get range at the first number to stay as is eliminating smaller numbers
+        addNum = 1;//loop counter and index filler
+        while(addNum < 10 - amount)//will not run if 9 is the amount
+        {
+            data.Add(addNum);//re inserts smaller numbers at the end starting with 1 and going up each loop
+            addNum += 1;//changes number added next loop or ends loop
+        }
+        return data;//returns modified form of data for rotate right
+        // TODO Problem 2 Start (don't forget to fill out the 01-prove-response.docx)
+    }
+}
